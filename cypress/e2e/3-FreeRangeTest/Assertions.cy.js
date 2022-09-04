7873b8c52f6140912c0eb7b815dcf0cc79c4207b
Nicole M Perez <NicoleMariePA@outlook.es>
2022-09-04 18:55:56 -0400

    Promesas en Cypress

--- cypress/e2e/3-FreeRangeTest/Assertions.cy.js
+++ cypress/e2e/3-FreeRangeTest/Assertions.cy.js
@@ -13,4 +13,35 @@ describe('Pruebas Manejo ', ()=>{
         expect('Inpusts').to.equal('Inpusts');
 
     })
+
+    it('Probando las promesas en cypress', ()=>{
+        let waited = false;
+
+        function waitOneSecond(){
+            //Devuelve una promesa que se da 
+            //por resuelta al pasar un segundo
+            return new
+            Cypress.Promise((resolve, reject)=>{
+                setTimeout(()=>{
+                    //ponemos en true la variable waited
+                    waited = true;
+                    //resolvemos con el string 'foo
+                    resolve('foo');
+                }, 1000);
+            });
+        }
+
+        cy.wrap(null).then(()=>{
+            //Devuelve una promesa a cy.then que
+            // es esperada hasta que se resuelva
+            return waitOneSecond().then((str)=>{
+                expect(str).to.equal('foo');
+                expect(waited).to.be.true;
+            });
+        });
+
+
+
+    })
+
 })
